8daab97d0c531d418d3f3a533f59bddfcf32ed4f
Diana Palafox <dianapalafox95@gmail.com>
2023-03-07 13:53:37 -0700

    Add timer

---
+++ game/src/components/Header.js
@@ -0,0 +1,12 @@
+import Timer from "./Timer";
+
+function Header() {
+    return (
+      <div className="Header">
+        <h1 className="title">Search and Find</h1>
+        <Timer />
+      </div>
+    );
+  }
+  
+  export default Header;
---
+++ game/src/components/Timer.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+
+function Timer() {
+    const [seconds, setSeconds] = useState(0)
+    const [minutes, setMinutes] = useState(0)
+    const [hours, setHours] = useState(0)
+
+   
+    useEffect(() => {
+       let timer = setInterval(() => {
+            setSeconds(seconds + 1)
+            if(seconds=== 59) {
+                setMinutes(minutes+1)
+                setSeconds(0)
+            }else if (minutes === 59){
+                setHours(hours+1)
+                setMinutes(0)
+                setSeconds(0)
+            }
+        }, 1000)
+        return () => clearInterval(timer)
+
+    })
+
+
+    return (
+      <div className="timer">
+        <div className="timer-container">
+            <div className="timer-box">
+                <span>{hours <10 ? '0' + hours : hours}:{minutes < 10 ? '0' + minutes : minutes}:{seconds < 10 ? '0' + seconds : seconds}</span>
+            </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Timer;
