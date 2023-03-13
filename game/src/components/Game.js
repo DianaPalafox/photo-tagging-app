8863473b63320186fc5be7fec36d9eed629582f7
Diana Palafox <dianapalafox95@gmail.com>
2023-03-13 15:42:57 -0700

    Add logic for founding character

--- game/src/components/Game.js
+++ game/src/components/Game.js
@@ -1,7 +1,10 @@
 import './Game.css';
 import React from "react";
 import { useState } from "react";
-import Menu from './Menu';
+import { doc, getDoc } from "firebase/firestore";
+import ConnectToDatabase from '../firebase';
+
+const db = ConnectToDatabase();
 
 function Game() {
     const [menu, setMenu] = useState(false)
@@ -9,17 +12,79 @@ function Game() {
     const [y, setY] = useState(0)
     const [coords, setCoords] = useState({x: 0, y: 0});
 
-    function toogleMenu(e) {
-        setMenu(prevMenu => !prevMenu);
-        e.nativeEvent.offsetX + 130 > window.innerWidth ? setX(`${window.innerWidth - 140}px`) : setX(e.nativeEvent.offsetX);
-        e.nativeEvent.offsetY + 150 > window.innerHeight ? setY(`${window.innerHeight - 180}px`) : setY(e.nativeEvent.offsetY)
-        setCoords({x:e.nativeEvent.offsetX, y:e.nativeEvent.offsetY})
-        console.log(x, y)
+    function handleCoordinates(e) {
+        const xCoord = Math.round((e.nativeEvent.offsetX / e.nativeEvent.target.offsetWidth) * 100);
+        const yCoord = Math.round((e.nativeEvent.offsetY / e.nativeEvent.target.offsetHeight) * 100);
+        handleMenu(e)
+        setCoords({
+            x: xCoord,
+            y: yCoord,
+        })
+        console.log(coords)
     }
 
+    function handleMenu(e){
+        e.nativeEvent.offsetX + 130 > window.innerWidth ? setX(`${window.innerWidth - 140}px`) : setX(e.nativeEvent.offsetX);
+        e.nativeEvent.offsetY + 150 > window.innerHeight ? setY(`${window.innerHeight - 280}px`) : setY(e.nativeEvent.offsetY)
+        setMenu(prevMenu => !prevMenu);
+    }
+
+    function getCharCoordData(characterName) {
+        const docRef = doc(db, "coordenadas", `${characterName}`);
+        let docSnap;
+        const charCoords = (async () => {
+          docSnap = await getDoc(docRef);
+          let coordData = null;
+          if (docSnap.exists()) {
+            coordData = docSnap.data();
+          }
+          return coordData;
+        })();
+        return charCoords;
+      }
+
+
+      function characterSelected(e) {
+        const characterName = e.target.id;
+        const charCoordData = getCharCoordData(characterName);
+
+        const checkClickedCoords = async () => {
+            const charCoords = await charCoordData;
+            if(
+                (charCoords.x === coords.x || charCoords.x + 1 === coords.x ||
+                charCoords.x - 1 === coords.x) && (charCoords.y === coords.y || 
+                charCoords.y + 1 === coords.y || charCoords.y - 1 === coords.y)
+            ){
+                console.log('found')
+                setMenu(false)
+            } else {
+                console.log('not found')
+                setMenu(false)
+            }        
+        }
+        checkClickedCoords();
+      }
+
     return (
-      <div className="game" onClick={(e) => toogleMenu(e)} >
-        <Menu x = {x}  y = {y} menu = {menu} coords = {coords}/>
+      <div className="game" onClick={(e) => handleCoordinates(e)} >
+        {menu && <div className="menu" style={{
+            position: 'absolute',
+            left: x,
+            top: y,
+        }}>
+            <button className="img1" id="character1" onClick={characterSelected}>
+                <div className="box-img1" id="character1"></div>
+                <p className="char1" id="character1">Character 1</p>
+            </button>
+            <button className="img1" id="character2" onClick={characterSelected}>
+                <div className="box-img2" id="character2"></div>
+                <p className="char2" id="character2">Character 2</p>
+            </button>
+            <button className="img1" id="character3" onClick={characterSelected}>
+                <div className="box-img3" id="character3"></div>
+                <p className="char3" id="character3">Character 3</p>
+            </button>
+        </div>}
         <div className='credit'><p>Image by Marija Tiurina</p></div>
       </div>
     );
--- game/src/firebase.js
+++ game/src/firebase.js
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+export default function ConnectToDatabase() {
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyB_dNo2PwggwiQ98TOgE3ONA0vzgxnmqSc",
@@ -15,3 +16,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+return db;
+
+}
